e7b5e158ab13c3b2b942581aec91867b928759ae
Apprehensivebus <142690685+Apprehensivebus@users.noreply.github.com>
2023-08-23 12:41:23 +0200

    Added gitignore

--- catbot/src/main/java/catgirlbot/Main.java
+++ catbot/src/main/java/catgirlbot/Main.java
@@ -1,4 +1,5 @@
 package catgirlbot;
+
 import java.time.LocalTime;
 
 import org.javacord.api.*;
@@ -10,10 +11,9 @@ import org.javacord.api.entity.message.MessageAuthor;
 
 public class Main {
      public static void Meow(TextChannel channel, MessageAuthor author, int delay)throws InterruptedException {
-        channel.sendMessage("Okey, I\'ll meow at you in "+delay+" seconds, it is currently "+LocalTime.now());
+        channel.sendMessage("Okey, I\'ll meow at you in " + delay + " seconds, it is currently " + LocalTime.now());
         Thread.sleep(delay*1000);
-        author.asUser().ifPresent(user -> channel.sendMessage("Meow~, "+user.getMentionTag()+", it has been "+delay+" seconds, it is now "+LocalTime.now()));
-        ;
+        author.asUser().ifPresent(user -> channel.sendMessage("Meow~, " + user.getMentionTag() + ", it has been " + delay + " seconds, it is now " + LocalTime.now()));
     }
     
     public static void main(String[] args) {
@@ -21,19 +21,36 @@ public class Main {
         String token = System.getenv("SECONDDISTOKEN");
         DiscordApi api = new DiscordApiBuilder().setToken(token).addIntents(Intent.MESSAGE_CONTENT).login().join();
         api.addMessageCreateListener(event -> {
-            if (event.getMessageContent().length()>14){
-            if (event.getMessageContent().substring(0, 14).equalsIgnoreCase("Mrrp at me in ")) {
-                try{
-                    int delay = Integer.parseInt(event.getMessageContent().substring(14, event.getMessageContent().length()));
-                    if (delay>-1){
-                    new Thread(){public void run() {try {Meow(event.getChannel(), event.getMessageAuthor(), delay);} catch (InterruptedException e) {e.printStackTrace();}}}.start();
-                }   else{ event.getChannel().sendMessage("That...... is not a positive number");
-            }}
-                catch(Exception e){
+
+            // block for meow timer
+            if (event.getMessageContent().substring(0, Math.min(event.getMessageContent().length(), 14)).equalsIgnoreCase("Mrrp at me in ")) {
+                try {
+                    int delay = Integer.parseInt(event.getMessageContent().replaceAll(",", "").substring(14));
+                    if (delay>-1) {
+                        new Thread(){
+                            public void run() {
+                                try {
+                                    Meow(event.getChannel(), event.getMessageAuthor(), delay);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }.start();
+                    } else {
+                        event.getChannel().sendMessage("That...... is not a positive number");
+                    }
+                } catch (Exception e) {
                     event.getChannel().sendMessage("That...... is not a positive number"); // catch the exception when people give non--numbers as timer delay
                 }
-            }}
+            }
+            // end of block for meow timer
+
+            // begin general block for commands starting with catgirl,
+            if (event.getMessageContent().substring(0, Math.min(event.getMessageContent().length(), 9)).equalsIgnoreCase("catgirl, ")) {
+                event.getChannel().sendMessage("Command test, received:");
+
+            }
+            // end general block for commands starting with catgirl,
         });
     }
-
 }
