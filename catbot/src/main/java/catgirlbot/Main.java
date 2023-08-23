f150ab59453a0aca7f7d8a2ab683f4a857d379ac
Apprehensivebus <142690685+Apprehensivebus@users.noreply.github.com>
2023-08-23 12:58:59 +0200

    meow?

--- catbot/src/main/java/catgirlbot/Main.java
+++ catbot/src/main/java/catgirlbot/Main.java
@@ -1,20 +1,13 @@
 package catgirlbot;
 
-import java.time.LocalTime;
-
 import org.javacord.api.*;
-import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.intent.Intent;
-import org.javacord.api.entity.message.MessageAuthor;
+
 
 
 
 public class Main {
-     public static void Meow(TextChannel channel, MessageAuthor author, int delay)throws InterruptedException {
-        channel.sendMessage("Okey, I\'ll meow at you in " + delay + " seconds, it is currently " + LocalTime.now());
-        Thread.sleep(delay*1000);
-        author.asUser().ifPresent(user -> channel.sendMessage("Meow~, " + user.getMentionTag() + ", it has been " + delay + " seconds, it is now " + LocalTime.now()));
-    }
+
     
     public static void main(String[] args) {
         // Insert your bot's token here
@@ -30,7 +23,7 @@ public class Main {
                         new Thread(){
                             public void run() {
                                 try {
-                                    Meow(event.getChannel(), event.getMessageAuthor(), delay);
+                                    Meow.meow(event.getChannel(), event.getMessageAuthor(), delay);
                                 } catch (InterruptedException e) {
                                     e.printStackTrace();
                                 }
--- catbot/src/main/java/catgirlbot/Meow.java
+++ catbot/src/main/java/catgirlbot/Meow.java
@@ -1,6 +1,15 @@
 package catgirlbot;
 
+import java.time.LocalTime;
+
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.MessageAuthor;
+
 public class Meow{
-    
+        public static void meow(TextChannel channel, MessageAuthor author, int delay)throws InterruptedException {
+            channel.sendMessage("Okey, I\'ll meow at you in " + delay + " seconds, it is currently " + LocalTime.now());
+            Thread.sleep(delay*1000);
+            author.asUser().ifPresent(user -> channel.sendMessage("Meow~, " + user.getMentionTag() + ", it has been " + delay + " seconds, it is now " + LocalTime.now()));
+        }
    
 }
