aca28d92c13f8df197bd07d91d134fa10774f48b
Apprehensivebus <142690685+Apprehensivebus@users.noreply.github.com>
2023-08-24 15:19:34 +0200

    can now load and save perms which only made me cry 7 times

--- catbot/src/main/java/catgirlbot/Main.java
+++ catbot/src/main/java/catgirlbot/Main.java
@@ -1,45 +1,48 @@
 package catgirlbot;
 
+
+
 import java.util.BitSet;
 import java.util.HashMap;
 
 import org.javacord.api.*;
 import org.javacord.api.entity.intent.Intent;
+import org.json.JSONObject;
 
 
 
 
 public class Main {
-    static Long homechannel = 1142829953562464267L; // temp solution until I add commands to add channels and file storage for persistence
-    static BitSet homeperms= new BitSet(2);
-
-    public static HashMap<Long, BitSet> initPerms(){ //creates the permission bitset for each channel
-        HashMap<Long, BitSet> perms= new HashMap<Long, BitSet>(); 
-        homeperms.set(1);
-        homeperms.set(0);
-        perms.put(homechannel,homeperms); // woo bitset
-        return perms;
-    }
+    public static JSONObject perms;
     
+    
+
     public static void main(String[] args) {
         // Insert your bot's token here
         String token = System.getenv("SECONDDISTOKEN");
         DiscordApi api = new DiscordApiBuilder().setToken(token).addIntents(Intent.MESSAGE_CONTENT).login().join();
-        HashMap<Long, BitSet> perms=initPerms();
+        JSONObject perms = Perms.initPerms();
+        System.out.println(perms.isEmpty());
+        HashMap<Long, BitSet> parsedPerms = Perms.parsePerms(perms);
+
         api.addMessageCreateListener(event -> {
-            if (perms.containsKey(event.getChannel().getId())){
+            if (parsedPerms.containsKey(event.getChannel().getId())){
 
                 // block for meow timer
-                if (perms.get(event.getChannel().getId()).get(0)&&event.getMessageContent().substring(0, Math.min(event.getMessageContent().length(), 14)).equalsIgnoreCase("Meow at me in ")) {
+                if (parsedPerms.get(event.getChannel().getId()).get(0)&&event.getMessageContent().substring(0, Math.min(event.getMessageContent().length(), 14)).equalsIgnoreCase("Meow at me in ")) {
                     Meow.meowtimer(event);
                 }
             // end of block for meow timer
 
-            // begin general block for commands starting with catgirl,
-                if (perms.get(event.getChannel().getId()).get(1)&&event.getMessageContent().substring(0, Math.min(event.getMessageContent().length(), 9)).equalsIgnoreCase("catgirl, ")) {
-                    if (event.getMessageContent().substring(9).equalsIgnoreCase("treasure hunt")) {
+            // begin general block for commands starting with catgirl, 
+                if (event.getMessageContent().substring(0, Math.min(event.getMessageContent().length(), 9)).equalsIgnoreCase("catgirl, ")) {
+                    if (parsedPerms.get(event.getChannel().getId()).get(1)&&event.getMessageContent().substring(9).equalsIgnoreCase("treasure hunt")) {
                         new Treasurehunt(event, api).start();
                     }
+                }
+
+                if (event.getMessageContent().equalsIgnoreCase("catgirl, save settings")) {
+                    Perms.savePerms(perms);
                 }
             // end general block for commands starting with catgirl,
             }
---
+++ catbot/src/main/java/catgirlbot/Perms.java
@@ -0,0 +1,62 @@
+package catgirlbot;
+
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.BitSet;
+import java.util.HashMap;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+public class Perms {
+    static Long homechannel = 1142829953562464267L; // temp solution until I add commands to add channels and file storage for persistence
+    static BitSet homeperms= new BitSet(2);
+    static BitSet permSet = new BitSet();
+
+    public static HashMap<Long, BitSet> parsePerms(JSONObject perms){
+        HashMap<Long, BitSet> parsedPerms =  new HashMap<Long, BitSet>();
+        for (String key : perms.keySet()){
+            permSet.clear();
+            parsedPerms.put(Long.parseLong(key),(BitSet) perms.get(key));
+        }
+        return parsedPerms;
+    }
+
+    public static JSONObject initPerms(){ //creates the permission bitset for each channel
+        JSONObject perms= new JSONObject(); // JSONobject with channel id as key and perms 
+        //homeperms.set(0);// enable first perm by default for now
+        homeperms.set(1);// enable second perm by default for now
+        perms.put(String.valueOf(homechannel),homeperms); // woo bitset
+        return perms;
+    }
+
+    public static JSONObject loadPerms(){ //creates the permission bitset for each channel
+        InputStream is;
+        try {
+            is = new FileInputStream("perms.json");
+            String jsonTxt = IOUtils.toString(is, "UTF-8");
+            JSONObject perms = new JSONObject(jsonTxt); 
+            System.out.println("Successfully loaded settings");
+            System.out.println(perms.get(String.valueOf(homechannel)));
+            return perms;
+        } catch (Exception e) {
+            JSONObject perms=initPerms();
+            e.printStackTrace();
+            System.out.println("Unsuccessfully loaded settings");
+            return perms;
+        }
+    }
+    
+    public static void savePerms(JSONObject perms){
+        try {
+            FileWriter myWriter = new FileWriter("perms.json");
+            perms.write(myWriter);
+            myWriter.close();
+            System.out.println("Successfully saved settings");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
