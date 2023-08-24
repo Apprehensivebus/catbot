d95028a3135a71c72e8d440b43b76470eac1906e
Apprehensivebus <142690685+Apprehensivebus@users.noreply.github.com>
2023-08-24 13:14:19 +0200

    replaced perms string with bitset

--- catbot/src/main/java/catgirlbot/Main.java
+++ catbot/src/main/java/catgirlbot/Main.java
@@ -1,5 +1,6 @@
 package catgirlbot;
 
+import java.util.BitSet;
 import java.util.HashMap;
 
 import org.javacord.api.*;
@@ -9,11 +10,14 @@ import org.javacord.api.entity.intent.Intent;
 
 
 public class Main {
-    static Long homechannel = 1142829953562464267L;
+    static Long homechannel = 1142829953562464267L; // temp solution until I add commands to add channels and file storage for persistence
+    static BitSet homeperms= new BitSet(2);
 
-    public static HashMap<Long, String> initPerms(){
-        HashMap<Long, String> perms= new HashMap<Long, String>(); 
-        perms.put(homechannel,"11"); 
+    public static HashMap<Long, BitSet> initPerms(){ //creates the permission bitset for each channel
+        HashMap<Long, BitSet> perms= new HashMap<Long, BitSet>(); 
+        homeperms.set(1);
+        homeperms.set(0);
+        perms.put(homechannel,homeperms); // woo bitset
         return perms;
     }
     
@@ -21,18 +25,18 @@ public class Main {
         // Insert your bot's token here
         String token = System.getenv("SECONDDISTOKEN");
         DiscordApi api = new DiscordApiBuilder().setToken(token).addIntents(Intent.MESSAGE_CONTENT).login().join();
-        HashMap<Long, String> perms=initPerms();
+        HashMap<Long, BitSet> perms=initPerms();
         api.addMessageCreateListener(event -> {
             if (perms.containsKey(event.getChannel().getId())){
 
                 // block for meow timer
-                if (perms.get(event.getChannel().getId()).charAt(0)=='1'&&event.getMessageContent().substring(0, Math.min(event.getMessageContent().length(), 14)).equalsIgnoreCase("Meow at me in ")) {
+                if (perms.get(event.getChannel().getId()).get(0)&&event.getMessageContent().substring(0, Math.min(event.getMessageContent().length(), 14)).equalsIgnoreCase("Meow at me in ")) {
                     Meow.meowtimer(event);
                 }
             // end of block for meow timer
 
             // begin general block for commands starting with catgirl,
-                if (perms.get(event.getChannel().getId()).charAt(1)=='1'&&event.getMessageContent().substring(0, Math.min(event.getMessageContent().length(), 9)).equalsIgnoreCase("catgirl, ")) {
+                if (perms.get(event.getChannel().getId()).get(1)&&event.getMessageContent().substring(0, Math.min(event.getMessageContent().length(), 9)).equalsIgnoreCase("catgirl, ")) {
                     if (event.getMessageContent().substring(9).equalsIgnoreCase("treasure hunt")) {
                         new Treasurehunt(event, api).start();
                     }
